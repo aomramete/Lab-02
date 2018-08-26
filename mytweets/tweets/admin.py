bc5ebad45988100cfce8ebad8c4a9570c33f9456
aomramete <32474279+aomramete@users.noreply.github.com>
2018-08-26 12:25:53 +0700

    Add files via upload

---
+++ mytweets/tweets/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from tweets.models import Tweet
+admin.site.register(Tweet)
---
+++ mytweets/tweets/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+from user_profile.models import User
+from tweets.models import Tweet
+
+# Create your views here.
+
+
+class Index(View):
+    def get(self, request):
+        params = {}
+        params["name"] = "Django"
+        return render(request, 'base.html', params)
+
+class Profile(View):
+   """User Profile page reachable from /user/<username> URL"""
+   def get(self, request, username):
+       params = dict()
+       user = User.objects.get(username=username)
+       tweets = Tweet.objects.filter(user=user)
+       params["tweets"] = tweets
+       params["user"] = user
+       return render(request, 'profile.html', params)
